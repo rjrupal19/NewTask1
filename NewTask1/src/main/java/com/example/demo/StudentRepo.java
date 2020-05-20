2b36ba7e88c0100cb421dd9697fdc34c916374d3
ADMIN <ADMIN@192.168.0.10>
2020-05-20 20:24:38 +0530

    added java docs for student repo interface.

--- NewTask1/src/main/java/com/example/demo/StudentRepo.java
+++ NewTask1/src/main/java/com/example/demo/StudentRepo.java
@@ -5,10 +5,14 @@ import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Student;
 
+/**
+ * it will do all the crud operations.
+ * @author Rupal Joshi
+ */
 @Repository
 public interface StudentRepo extends JpaRepository<Student, Integer>
 {
 	
-	
+					
 
 }
